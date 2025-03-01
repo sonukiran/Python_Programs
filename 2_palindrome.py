8062936812d48c367f6071fff143db6af1ec18aa
sonukiran <sonukiran44@gmail.com>
2025-03-01 21:40:17 +0530

    palindrome

---
+++ 2_palindrome.py
@@ -0,0 +1,8 @@
+#program to check whether the number or string is a palindrom
+
+num = input()
+
+if num == num [::-1]:
+    print("The number is a palindrome")
+else:
+    print("the number is not palindrome")
